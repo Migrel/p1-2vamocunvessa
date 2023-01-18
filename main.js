ae8419b37bb88b2192162361f5cd31acc0670eb7
Migrel <111578877+Migrel@users.noreply.github.com>
2023-01-18 18:45:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcUkvWdblgqFAZOm6xLhdPsaO3s8A16aw",
+    authDomain: "kiwitter-bfce8.firebaseapp.com",
+    projectId: "kiwitter-bfce8",
+    storageBucket: "kiwitter-bfce8.appspot.com",
+    messagingSenderId: "895400476335",
+    appId: "1:895400476335:web:192da2512d3ccc30685e5a",
+    measurementId: "G-MM8ZGRS9ZD"
+  };
+
+var SeuNome = ""
+
+function addUser(){
+    SeuNome = document.getElementById("userName").value;
+    localStorage.setItem("keyName", SeuNome);
+    window.location = "kwitterRoom.html";
+}
